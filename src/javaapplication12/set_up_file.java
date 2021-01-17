b726dab2527a7fe652d52726027a898ec05d1ec5
nakul <nakul@gmail.com>
2021-01-17 19:15:17 +0530

    commit

---
+++ src/javaapplication12/set_up_file.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nakul
+ */
+public class set_up_file 
+
+{
+    public static void main(String args[])
+    {
+        new notepad().setVisible(true);
+    }
+}
